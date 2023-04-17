212ff019259fae945dc089c41ceefeff27fbd8aa
Johguxo <jgonzalesi@uni.pe>
2023-04-17 07:04:10 -0500

    change base url

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = 'https://tmmback-production.up.railway.app/'
-export const BASE_URL = 'http://10.0.2.2:8000/'
+export const BASE_URL = 'https://tmmback-production.up.railway.app/'
+//export const BASE_URL = 'http://10.0.2.2:8000/'
